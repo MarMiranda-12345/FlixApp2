eefe25632d1e0f9e4bc1e646ee250850029cb660
sorryForDisappointing <53985598+sorryForDisappointing@users.noreply.github.com>
2021-03-03 17:06:12 -0800

    Flix App 2
    This is an continuation of the Flix App.

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Mariano Miranda Herrera on 3/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
